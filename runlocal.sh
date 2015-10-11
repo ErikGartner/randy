4076958e51e7f2fc8bb25cc723777ade78dcdd07
Erik <xxxsmoizxxx@gmail.com>
2015-10-11 22:02:10 +0200

    chore: updated build system
    Added Gulp buildsystem and removed old shell scripts.

--- runlocal.sh
+++
@@ -1,9 +0,0 @@
-cd app
-
-echo "Setting development packages"
-meteor add accounts-password
-meteor remove accounts-facebook
-meteor add msavin:mongol
-
-echo "Starting server"
-meteor run --settings settings.json
